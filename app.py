4b666f226b0d2ec74dff87f091592b4446ad7a45
janette-jerusal <janette.jerusal2@rtx.com>
2025-06-26 15:29:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import io
 
+results_df = pd.DataFrame()
+
 # Page Config 
 st.set_page_config(page_title="User Story Matcher", layout="wide")
 st.markdown("""
