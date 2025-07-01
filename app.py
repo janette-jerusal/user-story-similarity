c8b5f52d238eaa96febb5ef0ea764e91e978bf84
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-01 10:40:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def compute_similarity(df1, df2, threshold):
 
 def convert_df_to_excel(df):
     output = io.BytesIO()
-    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+    with pd.ExcelWriter(output, engine="openpyxl") as writer:
         df.to_excel(writer, index=False, sheet_name="Top Matches")
     return output.getvalue()
 
